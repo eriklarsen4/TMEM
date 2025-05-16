5e0dd34d44096ffdff12fb26b81c5e2a31d099f4
eriklarsen4 <eriklarsen@email.arizona.edu>
2025-05-16 12:58:42 -0700

    reference formatting
    removed unrecognized field from DESCRIPTION file that references the paper from which the data files came (i.e. removed '@source [Larsen et al., (2022)](<doi ... >)' from the vignette and package file

--- R/TMEM.R
+++ R/TMEM.R
@@ -7,4 +7,4 @@
 #'    \item{aDRG_TPM}{a dataframe of DESeq2's computed normalized counts across 2 groups (Wild-Type and Mutant) of 4 samples of bulk RNA derived from mouse dorsal root ganglion neurons}
 #'    \item{aDRG_DEG_list}{a character vector containing the mm9 Entrez GeneIDs of the genes DESeq2 identified as differentially expressed}
 #' }
-#' @source [Larsen et al (2022)](<doi:10.1097/j.pain.0000000000002452>)
+#' @source [Larsen et al., (2022)](<doi:10.1097/j.pain.0000000000002452>)
