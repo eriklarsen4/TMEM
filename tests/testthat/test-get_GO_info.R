f8c57f85d5488578bd7496426a3b7c11e96ddba4
eriklarsen4 <eriklarsen@email.arizona.edu>
2025-05-24 22:31:06 -0700

    autocheck changes
    again, cut down on tests; removed most text from the comments file; corrected the "license" warning

--- tests/testthat/test-get_GO_info.R
+++ tests/testthat/test-get_GO_info.R
@@ -5,5 +5,4 @@ testthat::test_that("get_GO_info requires valid species strings", {
 testthat::test_that("get_GO_info requires at least one valid gene ID", {
   testthat::expect_no_error(get_GO_info(list_of_interest = c("TMEM184B", "ERIKLARSEN"), species = 'HS'))
   testthat::expect_error(get_GO_info(list_of_interest = c("MARTHAB", "ERIKLARSEN"), species = "HS"))
-  testthat::expect_error(get_GO_info(list_of_interest = '', species = 'HS'))
 })
